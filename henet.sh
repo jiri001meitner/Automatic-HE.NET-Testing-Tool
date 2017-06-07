db52aa6e326b0c01364651830271c2aec3cababc
jiri001meitner <jiri001meitner@gmail.com>
2017-06-07 08:36:58 +0200

    St čen  7 08:36:58 CEST 2017

--- henet.sh
+++ henet.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-version=0.1
+version=0.2
+logdir=/tmp/.testy #/path/to/logdir, change it
+installdir=/mnt/sdcard/bin/henet #/path/to/henet, change it
+cronfile=/etc/cron.d/henet
+henet="$0"
 
-logdir=/tmp/.testy
-DIR=/path/to/henet
-
+if [ -e "$cronfile" ] ; then
+	printf "" ; else
+	echo "installing cron"	;
+	echo "*/15 * * * * root \""$henet"\"" > /etc/cron.d/henet ;
+	/etc/init.d/cron restart	;
+    fi
+	
 if [ -e "${logdir}"/log.txt ] ; then
-	"${DIR}"/henet-main.sh &> "${logdir}"/log.txt ; else
+	"${installdir}"/henet-main.sh &> "${logdir}"/log.txt ; else
 	mkdir "${logdir}" ; 
-	touch "${logdir}"/log.txt ; fi
+	touch "${logdir}"/log.txt ; 
+	"${installdir}"/henet-main.sh &> "${logdir}"/log.txt ;
+	fi
 
-"${DIR}"/henet-main.sh &> "${logdir}"/log.txt
 exit 0
-    
-    
